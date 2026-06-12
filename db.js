3b8645dcafbea501a6a8c950fc9e61bf25a20ee6
Afonso <afonsomontes@live.com.pt>
2026-06-12 22:54:20 +0200

    Modifs pour Docker

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2/promise");
 // Crée un pool de connexions MySQL utilisé par toute l'application
 // Le pool permet de limiter le nombre de connexions simultanées et d'améliorer les performances
 const pool = mysql.createPool({
-    host: "localhost",
+    host: "mysql",
     user: "root",
     password: "root",
     database: "music_app"
